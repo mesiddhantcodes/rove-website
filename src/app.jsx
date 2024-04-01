fc8dee51ad388ceb1fc4ecda6d765b88a6113418
Siddhant Kumar <siddhuyadav662@gmail.com>
2024-04-01 16:50:18 +0530

    Refactor login button and update styling in app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -5,17 +5,17 @@ export default function App() {
 
   return (
     <>
-      <div class='bg-white flex flex-col justify-center items-center h-screen'>
-        <p class="text-black text-4xl">Welcome to ROVE</p>
-        {/* img */}
+      <div class="bg-white min-h-screen flex flex-col justify-center items-center py-8 px-4">
+        <p class="text-black text-4xl mb-4">Welcome to ROVE</p>
+        {/* Image */}
+        <img src="./src/assets/rove.png" alt="" class="h-32 w-32 mb-4" />
 
-        {/* images from the assets folder */}
-        <img src="./src/assets/rove.png" alt="" class="h-32 w-32" />
-
-        <Link href={'/login'}>
-          <button class="p-2 pr-4 pl-4 text-white bg-black rounded-md">Login</button>
-        </Link>
-
+        <div class="flex flex-col items-center">
+          {/* Login button */}
+          <Link href={'/login'}>
+            <button class="px-6 py-3 text-white bg-black rounded-md hover:bg-gray-800 transition-colors duration-300">Login</button>
+          </Link>
+        </div>
       </div>
 
     </>
