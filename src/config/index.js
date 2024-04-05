e56443839b3e5218596d8819a490869720c43f2c
Siddhant Kumar <siddhuyadav662@gmail.com>
2024-04-05 17:55:30 +0530

    Update backend URI in config/index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,3 @@
-export default { BACKEND_URI: "http://localhost:3000/v1" };
+export default { DEV_BACKEND_URI: "http://localhost:3000/v1",
+BACKEND_URI:"https://travel-made-easy.onrender.com/v1"
+};
